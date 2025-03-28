fc949bdd50b2adb7431208a9b40ab2c71e6ed44b
frizx1153 <frizx1153@gmail.com>
2025-03-28 13:57:14 +0530

    jhgsgadjhjasjhhdas

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ from .scrappers.rarbg import searchRarbg, getRarbgTorrentData
 import os
 app = FastAPI()
 
+PORT = int(os.getenv("PORT", 8000))
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
